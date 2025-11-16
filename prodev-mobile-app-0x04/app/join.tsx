01bfd6ab1f0ff8413f7c50c23087dec94e708fe9
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-17 00:41:18 +0100

    .

--- prodev-mobile-app-0x04/app/join.tsx
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -1,4 +1,4 @@
-import { Text, TextInput, View, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { FACEBOOKLOGO, GOOGLELOGO, HEROLOGOGREEN } from "@/constants";
