e918018aa484a65059ad859925e8972d8ad55154
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-16 23:38:21 +0100

    .

--- prodev-mobile-app-0x04/app/_layout.tsx
+++ prodev-mobile-app-0x04/app/_layout.tsx
@@ -1,12 +1,11 @@
 import { Stack } from "expo-router";
-import { View } from 'react-native'
 
 export default function RootLayout() {
   return (
     <Stack screenOptions={{
       headerShown: false,
     }}>
-      <Stack.Screen name="/" />
+      <Stack.Screen name="index" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
     </Stack>
