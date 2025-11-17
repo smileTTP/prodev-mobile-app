12ecd80db927a4852af04f0bf7bf141b4a71f86a
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-17 14:07:37 +0100

    home pages layout

---
+++ prodev-mobile-app-0x05/app/(home)/inbox.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Inbox = () => {
+    return (
+        <View>
+        <Text>Inbox page</Text>
+        </View>
+    )
+}
+
+export default Inbox;
---
+++ prodev-mobile-app-0x05/app/(home)/saved.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Saved = () => {
+    return (
+        <View>
+        <Text>Saved posts page</Text>
+        </View>
+    )
+}
+
+export default Saved;
