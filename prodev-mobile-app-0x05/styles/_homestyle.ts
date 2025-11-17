3e16fba6da6c7d76c87d14acd5f6a6195ebace04
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-17 13:42:31 +0100

    .

---
+++ prodev-mobile-app-0x05/styles/_homestyle.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    searchGroup: {
+
+    },
+    searchFormGroup: {
+
+    },
+    searchControlGroup: {
+
+    },
+    searchFormText: {
+
+    },
+    searchControl: {
+
+    },
+    searchButton: {
+
+    },
+    filterGroup: {
+
+    },
+    filterContainer: {
+
+    },
+    listingContainer: {
+
+    },
+    paginationContainer: {
+
+    },
+    showMoreButton: {
+
+    },
+    showMoreButtonText: {
+
+    }
+
+});
+
+export { styles };
