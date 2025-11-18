f40243d2fe4443693beaefcacdc3d320b8f7dfe3
SmileTTP <ibtissamlazraf@gmail.com>
2025-11-18 22:36:35 +0100

    profile page

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -28,7 +28,7 @@ const HomeRootLayout = () => {
       }} />
         <Tabs.Screen name="profile" options={{
         title: 'Profile',
-        headerShown: true,
+        headerShown: false,
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
       }} />
     </Tabs>
